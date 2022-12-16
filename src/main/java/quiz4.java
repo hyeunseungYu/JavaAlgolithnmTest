1ee915dbf57936ab080c0610218eef293a5ed913
hyeunseungYu <hyeunseung03@gmail.com>
2022-12-16 15:43:01 +0900

    [update]ë²quiz4업데이트

---
+++ src/main/java/quiz4.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class quiz4 {
+    public static void main(String[] args) {
+        String[] participant = new String[]{"mislav", "stanko", "mislav", "ana"};
+        String[] completion = new String[]{"stanko", "ana", "mislav"};
+
+        String answer = "";
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (String s : participant){
+            System.out.println(s);
+        }
+        for (String s:completion){
+            System.out.println(s);
+        }
+
+        for (int i = 0; i < completion.length; i++){
+            if (!participant[i].equals(completion[i])){
+                answer = participant[i];
+                System.out.println(answer);
+                return;}
+
+        }
+        answer = participant[participant.length -1];
+        System.out.println(answer);
+    }
+
+}
+
+//ana mis mis stan
+//ana mis stan
