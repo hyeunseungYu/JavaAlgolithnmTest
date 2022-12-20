ec4f19a20dd1d23733f8547d345879b091c4fb38
hyeunseungYu <118980125+hyeunseungYu@users.noreply.github.com>
2022-12-20 16:53:55 +0900

    [level 1] Title: 예산, Time: 3.04 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.java
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -1,38 +1,6 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 class Solution {
     public int solution(int[] d, int budget) {
-        int max = 0;
-
-        int target = budget;
-
-        int[] dStream = Arrays.stream(d).sorted().toArray();
-
-        ArrayList<Integer> dList = new ArrayList<>();
-
-        for (int i : dStream) {
-            dList.add(i);
-        }
-
-        int left = budget;
-        int cnt = 0;
-
-        for (Integer integer : dList) {
-            if (left < 0){
-                break;
-            }else {
-                left -= integer;
-                cnt++;
-            }
-        }
-
         int answer = 0;
-
-        if (left < 0){
-            answer = cnt -1;
-        }else {
-            answer = cnt;
-        }
         return answer;
     }
 }
