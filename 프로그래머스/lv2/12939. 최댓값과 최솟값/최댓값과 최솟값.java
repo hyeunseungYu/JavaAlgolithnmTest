2d0728337bce57273b0215641c00a93823582dd7
hyeunseungYu <118980125+hyeunseungYu@users.noreply.github.com>
2023-02-16 21:42:07 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 21.94 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String s) {
+        String[] numbers = s.split(" ");
+
+
+        int min = Integer.parseInt(numbers[0]);
+        int max = Integer.parseInt(numbers[0]);
+
+        for (int i = 1; i < numbers.length; i++) {
+            int num = Integer.parseInt(numbers[i]);
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        String answer = "";
+        answer += min;
+        answer += " ";
+        answer += max;
+        return answer;
+    }
+}
