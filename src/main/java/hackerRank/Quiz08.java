57ddd6f57f70650f767d295f89faa854230deade
Hyeunseung <hyeunseung03@gmail.com>
2023-04-11 12:12:54 +0900

    hackerranck insertion sort - part 2

---
+++ src/main/java/hackerRank/Quiz08.java
@@ -0,0 +1,34 @@
+package hackerRank;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Quiz08 {
+    public static void main(String[] args) {
+        int n = 7;
+        List<Integer> arr = Arrays.asList(3, 4, 7, 5, 6, 2, 1 );
+
+        int[] convert = new int[arr.size()];
+
+        for (int i = 0; i < convert.length; i++) {
+            convert[i] = arr.get(i);
+        }
+
+        for (int i = 1; i < convert.length; i++) {
+            int key = convert[i];
+            int j = i - 1;
+
+            while (j >= 0 && convert[j] > key) {
+                convert[j + 1] = convert[j];
+                j--;
+            }
+            convert[j + 1] = key;
+
+            for (int k = 0; k < convert.length; k++) {
+                System.out.print(convert[k] + " ");
+            }
+            System.out.println();
+
+        }
+    }
+}
